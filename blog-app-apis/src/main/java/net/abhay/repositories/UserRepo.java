6c6e607a4f6531a62757c205afc8333b49143935
abhay3690 <abhay7777@gmail.com>
2024-05-16 17:54:58 +0530

    Commit the autogenrate password

--- blog-app-apis/src/main/java/net/abhay/repositories/UserRepo.java
+++ blog-app-apis/src/main/java/net/abhay/repositories/UserRepo.java
@@ -1,5 +1,7 @@
 package net.abhay.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import net.abhay.entities.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-
+	Optional<User> findByEmail(String email);
 }
