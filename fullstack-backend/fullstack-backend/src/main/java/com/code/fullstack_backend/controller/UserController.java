3a82846f3827009519e902dc9cdf4137b0b66007
abhay3690 <abhay7777@gmail.com>
2024-05-23 22:11:17 +0530

    pust and deleete

--- fullstack-backend/fullstack-backend/src/main/java/com/code/fullstack_backend/controller/UserController.java
+++ fullstack-backend/fullstack-backend/src/main/java/com/code/fullstack_backend/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.code.fullstack_backend.controller;
 
+import com.code.fullstack_backend.exception.UserNotFoundException;
 import com.code.fullstack_backend.model.User;
 import com.code.fullstack_backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 @RestController
@@ -20,5 +22,27 @@ public class UserController {
     @GetMapping("/user")
     List<User> getAllUser(){
         return userRepository.findAll();
+    }
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id).map(
+                user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+        }).orElseThrow(()->new UserNotFoundException(id));
+    }
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if (!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id"+id+"has been deleted success .";
     }
 }
--- fullstack-backend/fullstack-backend/src/main/java/com/code/fullstack_backend/repository/UserRepository.java
+++ fullstack-backend/fullstack-backend/src/main/java/com/code/fullstack_backend/repository/UserRepository.java
@@ -4,5 +4,5 @@ package com.code.fullstack_backend.repository;
 import com.code.fullstack_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
