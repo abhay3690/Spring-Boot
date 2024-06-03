e046bf67143d77da65eb1bbc36303ec682d1982d
abhay3690 <abhay7777@gmail.com>
2024-06-03 23:06:28 +0530

    Add

--- product/src/main/java/com/controller/ProductController.java
+++ product/src/main/java/com/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,7 @@ import com.payload.ProductDto;
 import com.service.ProductService;
 
 import jakarta.validation.Valid;
+import net.abhay.payloads.UserDto;
 
 @RestController
 @RequestMapping("/ ")
@@ -23,5 +27,9 @@ public class ProductController {
 		ProductDto createProductDto = this.productService.createProduct(productDto);
 		return new ResponseEntity<>(createProductDto, HttpStatus.CREATED);
 	}
-	
+	@GetMapping("/")
+	public ResponseEntity<List<ProductDto>> getAllUser() {
+		List<ProductDto> allUser = this.productService.allProduct();
+		return ResponseEntity.ok(allProduct);
+	}
 }
