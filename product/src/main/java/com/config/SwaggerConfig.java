b60c8c448cff335a31e37bbfabeacd3d4bb0df10
abhay3690 <abhay7777@gmail.com>
2024-06-06 15:02:39 +0530

    add Swagger

---
+++ product/src/main/java/com/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig implements WebMvcConfigurer {
+
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder().group("public-api").pathsToMatch("/api/**") // Specify the paths to match
+				.build();
+	}
+
+	@Bean
+	OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Product Application : BackEnd Api Project")
+						.version("1.0")
+						.description("Product & Seller API'S creating ")
+						.contact(new Contact()
+								.name("Abhay A Suthar")
+								.email("abhaysuthar7777@gmail.com")))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(
+						new Components()
+						.addSecuritySchemes("bearerAuth", new SecurityScheme()
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")));
+	}
+}
