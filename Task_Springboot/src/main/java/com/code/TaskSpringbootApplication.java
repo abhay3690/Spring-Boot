8c0a2739fa15e326aed30fe830271592ac6133e5
abhay3690 <abhay7777@gmail.com>
2024-07-22 22:58:24 +0530

    add UserController

--- Task_Springboot/src/main/java/com/code/TaskSpringbootApplication.java
+++ Task_Springboot/src/main/java/com/code/TaskSpringbootApplication.java
@@ -1,13 +1,19 @@
 package com.code;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskSpringbootApplication.class, args);
+	}
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
