2c702171bac4e6a72848902773a78e5747e030fd
abhay3690 <abhay7777@gmail.com>
2024-07-29 17:19:57 +0530

    Test cases

--- customResponse/src/main/java/data/entity/City.java
+++ customResponse/src/main/java/data/entity/City.java
@@ -1,15 +1,10 @@
 package data.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
