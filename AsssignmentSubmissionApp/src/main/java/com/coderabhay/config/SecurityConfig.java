6929c5db4fa240f030784bc6f799eb1eb9346c83
Abhay S <abhaysuthar7777@gmail.com>
2024-08-23 19:40:28 +0530

    add

--- AsssignmentSubmissionApp/src/main/java/com/coderabhay/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.coderabhay.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurer{
-
-}
